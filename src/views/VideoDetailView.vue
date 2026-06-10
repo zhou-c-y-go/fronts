00f99275889fc4d5c0d7f4d7a06d69f40cead01d
zhou-c-y-go <2553598443@qq.com>
2026-06-10 20:32:10 +0800

    收藏功能的添加

--- src/views/VideoDetailView.vue
+++ src/views/VideoDetailView.vue
@@ -68,12 +68,12 @@
                 target-type="video"
               />
 
-              <button @click="handleFavorite" class="flex items-center space-x-2 text-gray-400 hover:text-yellow-500 transition-colors group">
-                <div class="p-2.5 bg-gray-800 rounded-full group-hover:bg-yellow-500/20 transition-all">
-                  <svg class="w-5 h-5" :class="{'fill-current text-yellow-500': isFavorited}" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
-                </div>
-                <span class="font-bold">{{ videoDetail.favoriteCount || 0 }}</span>
-              </button>
+              <FavoriteButton
+                v-model:is-favorited="videoDetail.isFavorite"
+                v-model:favorite-count="videoDetail.favoriteCount"
+                target-type="video"
+                :target-id="videoDetail.id"
+              />
             </div>
           </div>
         </div>
