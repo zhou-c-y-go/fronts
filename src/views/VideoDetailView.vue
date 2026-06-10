0c9b10e16b2f67b4491f34ce1e33930c2eb8c753
zhou-c-y-go <2553598443@qq.com>
2026-06-10 20:31:55 +0800

    收藏功能的添加

--- src/views/VideoDetailView.vue
+++ src/views/VideoDetailView.vue
@@ -61,12 +61,12 @@
             </div>
 
             <div class="flex items-center space-x-6">
-              <button @click="handleLike" class="flex items-center space-x-2 text-gray-400 hover:text-pink-500 transition-colors group">
-                <div class="p-2.5 bg-gray-800 rounded-full group-hover:bg-pink-500/20 transition-all">
-                  <svg class="w-5 h-5" :class="{'fill-current text-pink-500': isLiked}" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-                </div>
-                <span class="font-bold">{{ videoDetail.likeCount || 0 }}</span>
-              </button>
+              <LikeButton
+                v-model:is-liked="videoDetail.isLiked"
+                v-model:like-count="videoDetail.likeCount"
+                :target-id="videoDetail.id"
+                target-type="video"
+              />
 
               <button @click="handleFavorite" class="flex items-center space-x-2 text-gray-400 hover:text-yellow-500 transition-colors group">
                 <div class="p-2.5 bg-gray-800 rounded-full group-hover:bg-yellow-500/20 transition-all">
